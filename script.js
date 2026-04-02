7b779fe6ae97bc5227c6ce452a50616aa82bcd09
AllMakeBO <moisesborbagato@gmail.com>
2026-04-02 15:31:54 -0300

    Refactor form handling and add sidebar toggle
    Removed form submission handling for steps 1 and 2. Added sidebar toggle functionality.

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-// Intercepta o envio do formulário do PASSO 1 (Email)
-document.getElementById('form-email').addEventListener('submit', function(event) {
-  // Impede que a página recarregue automaticamente ao clicar em "Obter código"
-  event.preventDefault(); 
-  
-  // Esconde a div do Passo 1 (trocando o display para 'none')
-  document.getElementById('step-1').style.display = 'none';
-  
-  // Mostra a div do Passo 2 (trocando o display para 'block')
-  document.getElementById('step-2').style.display = 'block';
-});
+const sidebar = document.getElementById('mySidebar');
+const openBtn = document.getElementById('openBtn');
 
-// Intercepta o envio do formulário do PASSO 2 (Código)
-document.getElementById('form-codigo').addEventListener('submit', function(event) {
-  // Impede que a página recarregue ao clicar em "Verificar código"
-  event.preventDefault();
-  
-  // Simulação da ação de envio para o back-end (Aqui entrará o Fetch/Axios para a API)
-  alert('Código enviado para verificação!'); 
-});
+let a = 0;
+
+function abrir_menu() {
+  if (a === 0) {
+    sidebar.classList.add('sidebar_open');
+    a = 1;
+    
+    document.getElementById("openBtn").innerHTML = "<strong> < </strong>";
+  } else {
+    sidebar.classList.remove('sidebar_open');
+    a = 0;
+    
+    document.getElementById("openBtn").innerHTML = "<strong> > </strong>";
+  }
+}
